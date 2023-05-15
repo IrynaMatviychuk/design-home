13d2f23e808d2810bfc49967e14f51f5fa9f6dd0
Iryna Borshchyk <ira011019997@gmail.com>
2023-05-15 14:46:59 +0300

    edit slider

--- app.js
+++ app.js
@@ -33,20 +33,4 @@ $(".slider").slick({
     slidesToScroll: 1,
     fade: true,
     speed: 4000,
-    // responsive: [
-    //     {
-    //       breakpoint: 1920,
-    //       settings: {
-    //         slidesToShow: 3,
-    //         slidesToScroll: 1,
-    //       }
-    //     },
-    //     {
-    //       breakpoint: 1024,
-    //       settings: {
-    //         slidesToShow: 2,
-    //         slidesToScroll: 1,
-    //       }
-    //     }
-    //   ]
   });
