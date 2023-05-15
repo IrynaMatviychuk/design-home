c34a5156162156322848e3f7371bd68fe3584b05
Iryna Borshchyk <ira011019997@gmail.com>
2023-05-16 01:19:55 +0300

    change assortement.css

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-// Add and open modal-block
+  // Add and open modal-block
 let modal = document.querySelector(".modal");
 let feedbackBtn = document.querySelector(".feedback");
 
 feedbackBtn.addEventListener("click", openModal);
 function openModal() {
-  modal.classList.add("show");
-  modal.classList.remove("hide");
+    modal.style.display = "block";
+    setTimeout(function() {
+        modal.classList.add("show");
+    }, 100);
 }
 
 // Close modal-block (click on btn-close (X))
@@ -13,24 +15,33 @@ let close = document.querySelector(".btn-close");
 
 close.addEventListener("click", closeModal);
 function closeModal() {
-  modal.classList.add("hide");
-  modal.classList.remove("show");
+    modal.classList.remove("show");
+    setTimeout(function() {
+        modal.style.display = "none";
+    }, 300);
 }
 
 // Close modal (click on modal-block)
 modal.addEventListener("click", function (e) {
-  if (e.target === modal) {
-    closeModal();
-  }
+    if (e.target === modal) {
+        closeModal();
+    }
 });
 
 // Slider
 $(".slider").slick({
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 2000,  
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true,
-    speed: 4000,
-  });
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 2000,  
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  speed: 4000,
+});
+
+// Get a reference to the input element
+const inputElement = document.querySelector('.form-field-email');
+
+inputElement.addEventListener('click', function() {
+  inputElement.classList.remove('form-field-email');
+});
