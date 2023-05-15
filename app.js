188e50cc097467e06a4d1b68aef0f6e3fa5740bb
Iryna Borshchyk <ira011019997@gmail.com>
2023-05-15 14:39:44 +0300

    change slide

--- app.js
+++ app.js
@@ -26,25 +26,27 @@ modal.addEventListener("click", function (e) {
 
 // Slider
 $(".slider").slick({
-    autoplay: true,
     dots: true,
-    slidesToShow: 4,
-    responsive: [
-        {
-          breakpoint: 1920,
-          settings: {
-            autoplay: true,
-            slidesToShow: 3,
-            // dots: true,
-          }
-        },
-        {
-          breakpoint: 1024,
-          settings: {
-            autoplay: true,
-            slidesToShow: 2,
-            // dots: true,
-          }
-        }
-      ]
+    autoplay: true,
+    autoplaySpeed: 2000,  
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    speed: 4000,
+    // responsive: [
+    //     {
+    //       breakpoint: 1920,
+    //       settings: {
+    //         slidesToShow: 3,
+    //         slidesToScroll: 1,
+    //       }
+    //     },
+    //     {
+    //       breakpoint: 1024,
+    //       settings: {
+    //         slidesToShow: 2,
+    //         slidesToScroll: 1,
+    //       }
+    //     }
+    //   ]
   });
